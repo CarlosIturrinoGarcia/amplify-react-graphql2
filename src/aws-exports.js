0f4accd3bdc38c74f2c3146415b7e23a45b71128
carlositurrino <carlos.iturrino@zetaminusone.com>
2023-12-15 09:36:30 -0400

    final commit

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -23,7 +23,11 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://axjis3fdp5edhbfhootbc5ok6a.appsync-api.us-west-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-west-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-tshom2absnfyxa52oogb6ddcuy"
 };
 
 
